5a54d26d090c0578589b8f5998dc98d5937633be
WelldoneDepot0437 <mboyiwelldone@gmail.com>
2025-04-23 17:28:43 +0200

    Mise en forme des pages

--- src/app/detail-transactions/detail-transactions.page.ts
+++ src/app/detail-transactions/detail-transactions.page.ts
@@ -2,25 +2,26 @@ import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import {
-  IonButton,
-  IonButtons, IonCol,
-  IonContent, IonGrid,
-  IonHeader,
-  IonIcon,
-  IonMenuButton, IonRow,
-  IonTitle,
-  IonToolbar, NavController
+    IonButton,
+    IonButtons, IonCard, IonCardContent, IonCol,
+    IonContent, IonGrid,
+    IonHeader,
+    IonIcon, IonLabel,
+    IonMenuButton, IonRow,
+    IonTitle,
+    IonToolbar, NavController
 } from '@ionic/angular/standalone';
 import {Router} from "@angular/router";
 import {arrowBack, arrowBackOutline, receiptOutline} from "ionicons/icons";
 import {addIcons} from "ionicons";
+import {ButtonDirective} from "primeng/button";
 
 @Component({
   selector: 'app-detail-transactions',
   templateUrl: './detail-transactions.page.html',
   styleUrls: ['./detail-transactions.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule, IonButton, IonButtons, IonIcon, IonMenuButton, IonGrid, IonRow, IonCol]
+  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule, IonButton, IonButtons, IonIcon, IonMenuButton, IonGrid, IonRow, IonCol, IonCard, IonCardContent, IonLabel, ButtonDirective]
 })
 export class DetailTransactionsPage implements OnInit {
 
